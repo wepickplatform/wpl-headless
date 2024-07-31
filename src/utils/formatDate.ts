43a8453628acc824521f92b428a51d6caf2dac47
wepickplatform <sanghyeon@wepick.kr>
2024-07-31 22:22:04 +0900

    Update formatDate.ts

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,3 +1,4 @@
+/*
 export default function ncFormatDate(date: string) {
   // CAN LAM THEM PHAN DATE FOrMAT ---- get_option(date_format)
   if (!date) return "";
@@ -7,4 +8,14 @@ export default function ncFormatDate(date: string) {
   let mo = new Intl.DateTimeFormat("en", { month: "short" }).format(d);
   let da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
   return `${mo} ${da}, ${ye}`;
+}
+*/
+export default function ncFormatDate(date: string) {
+  if (!date) return "";
+
+  let d = new Date(date);
+  let ye = d.getFullYear();
+  let mo = String(d.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 1을 더하고 두 자리로 만듦
+  let da = String(d.getDate()).padStart(2, '0'); // 날짜를 두 자리로 만듦
+  return `${ye}-${mo}-${da}`;
 }
