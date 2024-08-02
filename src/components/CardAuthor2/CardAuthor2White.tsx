ebc6c92c6d92a900245b66ac2125e0be20ebddd5
wepickplatform <sanghyeon@wepick.kr>
2024-08-02 16:52:58 +0900

    Update CardAuthor2White.tsx

--- src/components/CardAuthor2/CardAuthor2White.tsx
+++ src/components/CardAuthor2/CardAuthor2White.tsx
@@ -14,49 +14,7 @@ const CardAuthor2White: FC<CardAuthor2WhiteProps> = ({
 	readingTime,
 	date,
 }) => {
-<<<<<<< HEAD
-  const { databaseId, uri, name, featuredImageMeta } =
-    getUserDataFromUserCardFragment(
-      author as FragmentType<typeof NC_USER_FULL_FIELDS_FRAGMENT>
-    );
-  return (
-    <Link
-      href={uri}
-      className={`nc-CardAuthor2White relative inline-flex items-center ${className}`}
-    >
-      <Avatar
-        sizeClass="h-10 w-10 text-base"
-        containerClassName="flex-shrink-0 mr-3"
-        radius="rounded-full"
-        imgUrl={featuredImageMeta?.sourceUrl || ""}
-        userName={name || ""}
-      />
-      <div>
-        <h2
-          className={`text-sm text-neutral-200 hover:text-white dark:text-neutral-700 dark:hover:text-black font-medium capitalize`}
-        >
-          {name}
-        </h2>
-        <span
-          className={`flex items-center mt-1 text-xs text-neutral-300 dark:text-neutral-700`}
-        >
-          <span>{date}</span>
-          {readingTime && (
-            <>
-              <span className="mx-1 opacity-0 group-hover:opacity-100 transition-opacity">
-                ·
-              </span>
-              <span className="opacity-0 group-hover:opacity-100 transition-opacity">
-                {readingTime} 분
-              </span>
-            </>
-          )}
-        </span>
-      </div>
-    </Link>
-  );
-};
-=======
+
 	const { databaseId, uri, name, featuredImageMeta } =
 		getUserDataFromUserCardFragment(
 			author as FragmentType<typeof NC_USER_FULL_FIELDS_FRAGMENT>,
@@ -98,6 +56,5 @@ const CardAuthor2White: FC<CardAuthor2WhiteProps> = ({
 		</Link>
 	)
 }
->>>>>>> 0f186b6 (Save changes before rebase)
 
 export default CardAuthor2White
