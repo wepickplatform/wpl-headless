7a7b82f1244e4f769143354ac821e7c0ba74781a
wepickplatform <sanghyeon@wepick.kr>
2024-08-05 23:09:15 +0900

    Update index.tsx

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,358 +1,215 @@
-import { FaustPage, getNextServerSideProps } from '@faustwp/core'
+import { GetStaticPropsContext } from 'next'
+import { FaustPage, getNextStaticProps } from '@faustwp/core'
 import { gql } from '@/__generated__'
 import {
 	NcgeneralSettingsFieldsFragmentFragment,
-	OrderEnum,
-	PostObjectsConnectionOrderbyEnum,
-	PostsFilterPageQueryGetPostsQuery,
+	SearchPageQueryGetUsersBySearchQuery,
 } from '@/__generated__/graphql'
-import { FILTERS_OPTIONS } from '@/contains/contants'
+import { GET_USERS_FIRST_COMMON } from '@/contains/contants'
 import React from 'react'
+import ButtonPrimary from '@/components/Button/ButtonPrimary'
+import Empty from '@/components/Empty'
 import { useRouter } from 'next/router'
+import CardAuthorBox from '@/components/CardAuthorBox/CardAuthorBox'
+import { getUserDataFromUserCardFragment } from '@/utils/getUserDataFromUserCardFragment'
+import { useLazyQuery } from '@apollo/client'
 import { FOOTER_LOCATION, PRIMARY_LOCATION } from '@/contains/menu'
 import PageLayout from '@/container/PageLayout'
-import { PostDataFragmentType } from '@/data/types'
-import GridPostsArchive from '@/components/GridPostsArchive'
-import useGetPostsNcmazMetaByIds from '@/hooks/useGetPostsNcmazMetaByIds'
-import { TPostCard } from '@/components/Card2/Card2'
-import { GetServerSidePropsContext } from 'next'
-import TabFilters from '@/components/TabFilters'
+import errorHandling from '@/utils/errorHandling'
 import getTrans from '@/utils/getTrans'
-import { FireIcon } from '@/components/Icons/Icons'
-import ArchiveFilterListBox from '@/components/ArchiveFilterListBox/ArchiveFilterListBox'
+import { FireIcon, UserSearchIcon } from '@/components/Icons/Icons'
+import { UsersIcon } from '@heroicons/react/24/outline'
 
-const T = getTrans()
-const GET_POSTS_FIRST_COMMON = 24
-
-interface ConTextQuery {
-	search: string | null
-	tagIn: number[]
-	authorIn: number[]
-	categoryIn: number[]
-	field: string
-	order: string
-	first: number | null
-	last: number | null
-	after: string | null
-	before: string | null
-}
-
-const Page: FaustPage<PostsFilterPageQueryGetPostsQuery> = (props) => {
-	const { posts } = props.data || {}
+const Page: FaustPage<SearchPageQueryGetUsersBySearchQuery> = (props) => {
 	const router = useRouter()
+	const initUsers = props.data?.users?.nodes
+	const initPageInfo = props.data?.users?.pageInfo
+	const search = router.query.search?.[0] || ''
+	const T = getTrans()
 
-	//
-	const {} = useGetPostsNcmazMetaByIds({
-		posts: (posts?.nodes || []) as TPostCard[],
-	})
-	//
+	const [getUsersBySearch, getUsersBySearchResult] = useLazyQuery(
+		gql(` 
+      query queryGetUsersBySearchOnSearchPage(
+        $first: Int
+        $search: String
+        $after: String
+      ) {
+        users(first: $first, after: $after, where: { search: $search }) {
+          nodes {
+            ...NcmazFcUserFullFields
+          }
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
+        }
+      }
+    `),
+		{
+			notifyOnNetworkStatusChange: true,
+			context: {
+				fetchOptions: {
+					method: process.env.NEXT_PUBLIC_SITE_API_METHOD || 'GET',
+				},
+			},
+			variables: {
+				search,
+				first: GET_USERS_FIRST_COMMON,
+			},
+			onError: (error) => {
+				errorHandling(error)
+			},
+		},
+	)
 
-	const initPosts = (posts?.nodes as PostDataFragmentType[]) || []
-	const ctxQuery: ConTextQuery = props.__PAGE_VARIABLES__?.ctxQuery || {}
-
-	const onCategoriesUpdated = (ids: number[]) => {
-		router.push({
-			query: removeEmptyKey({
-				...ctxQuery,
-				categoryIn: ids,
-				first: null,
-				last: null,
-				after: null,
-				before: null,
-			}),
-		})
-	}
-
-	const onTagsUpdated = (ids: number[]) => {
-		router.push({
-			query: removeEmptyKey({
-				...ctxQuery,
-				tagIn: ids,
-				first: null,
-				last: null,
-				after: null,
-				before: null,
-			}),
-		})
-	}
-
-	const onAuthorsUpdated = (ids: number[]) => {
-		router.push({
-			query: removeEmptyKey({
-				...ctxQuery,
-				authorIn: ids,
-				first: null,
-				last: null,
-				after: null,
-				before: null,
-			}),
-		})
-	}
-
-	const onKeywordUpdated = (keyword: string) => {
-		router.push({
-			query: removeEmptyKey({
-				...ctxQuery,
-				search: keyword,
-				first: null,
-				last: null,
-				after: null,
-				before: null,
-			}),
-		})
-	}
-
-	const onClickNext = () => {
-		router.push({
-			query: removeEmptyKey({
-				...ctxQuery,
-				after: posts?.pageInfo.endCursor,
-				first: ctxQuery.first,
-				before: null,
-				last: null,
-			}),
-		})
-	}
-
-	const onClickPrev = () => {
-		router.push({
-			query: removeEmptyKey({
-				...ctxQuery,
-				first: null,
-				after: null,
-				before: posts?.pageInfo.startCursor,
-				last: ctxQuery.first,
-			}),
-		})
-	}
-
-	const removeEmptyKey = (obj: Record<string, any>) => {
-		Object.keys(obj).forEach((key) => !obj[key] && delete obj[key])
-		return obj
-	}
-
-	function checkRouterQueryFilter(
-		routerQueryFilter: `${PostObjectsConnectionOrderbyEnum}/${OrderEnum}`,
-	) {
-		// tra ve false neu khong co filter/ lan dau tien vao trang  / khi chua click vao filter nao
-		if (!routerQueryFilter) {
-			return false
+	const handleClickShowMore = () => {
+		if (!getUsersBySearchResult.called) {
+			return getUsersBySearch({
+				variables: {
+					search,
+					after: initPageInfo?.endCursor,
+				},
+			})
 		}
 
-		const [field, order] = routerQueryFilter?.split('/')
-		return {
-			field: field as PostObjectsConnectionOrderbyEnum,
-			order: order as OrderEnum,
-		}
+		getUsersBySearchResult.fetchMore({
+			variables: {
+				search,
+				after: getUsersBySearchResult.data?.users?.pageInfo.endCursor,
+			},
+			updateQuery: (prev, { fetchMoreResult }) => {
+				if (!fetchMoreResult || !fetchMoreResult.users?.nodes) {
+					return prev
+				}
+
+				return {
+					...prev,
+					users: {
+						...prev.users,
+						nodes: [
+							...(prev.users?.nodes || []),
+							...(fetchMoreResult.users?.nodes || []),
+						],
+						pageInfo: fetchMoreResult.users?.pageInfo,
+					},
+				}
+			},
+		})
+	}
+
+	// data for render
+	let currentUsers = initUsers || []
+	let hasNextPage = initPageInfo?.hasNextPage
+	let loading = false
+
+	if (getUsersBySearchResult.called) {
+		currentUsers = [
+			...(initUsers || []),
+			...(getUsersBySearchResult.data?.users?.nodes || []),
+		]
+
+		hasNextPage =
+			getUsersBySearchResult.loading ||
+			getUsersBySearchResult.data?.users?.pageInfo.hasNextPage ||
+			false
+		loading = getUsersBySearchResult.loading
 	}
 
 	return (
-		<>
-			<PageLayout
-				headerMenuItems={props.data?.primaryMenuItems?.nodes || []}
-				footerMenuItems={props.data?.footerMenuItems?.nodes || []}
-				pageFeaturedImageUrl={null}
-				pageTitle={T.Posts}
-				generalSettings={
-					props.data?.generalSettings as NcgeneralSettingsFieldsFragmentFragment
-				}
-			>
-				<div className={`nc-PageCollection`}>
-					<div className="container space-y-16 py-10 sm:space-y-20 lg:space-y-28 lg:pb-28 lg:pt-20">
-						<div className="space-y-10">
-							<header>
-								<div className="mb-2 flex items-center gap-2 text-sm font-medium text-neutral-500">
-									<FireIcon className="h-6 w-6" />
-									<span className="">{T['Explore']}</span>
-								</div>
-								<h1 className="block text-2xl font-semibold capitalize sm:text-3xl lg:text-4xl">
-									{T['Posts']}
-								</h1>
-							</header>
+		<PageLayout
+			headerMenuItems={props.data?.primaryMenuItems?.nodes || []}
+			footerMenuItems={props.data?.footerMenuItems?.nodes || []}
+			pageFeaturedImageUrl={null}
+			pageTitle={T['Authors']}
+			generalSettings={
+				props.data?.generalSettings as NcgeneralSettingsFieldsFragmentFragment
+			}
+		>
+			<div className="nc-PageExploreAuthors">
+				<div className="container space-y-16 py-10 sm:space-y-20 lg:space-y-28 lg:pb-28 lg:pt-20">
+					<div className="space-y-14">
+						<header>
+							<div className="mb-2 flex items-center gap-2 text-sm font-medium text-neutral-500">
+								<UsersIcon className="h-5 w-5" />
+								<span className="">{T['Explore']}</span>
+							</div>
+							<h1 className="block text-2xl font-semibold capitalize sm:text-3xl lg:text-4xl">
+								{T['Authors']}
+							</h1>
+						</header>
 
-							<hr className="border-slate-200 dark:border-slate-700" />
-
-							<main>
-								{/* TABS FILTER */}
-								<div className="flex flex-col lg:flex-row lg:justify-between">
-									<TabFilters
-										initCatIds={ctxQuery.categoryIn}
-										initTagIds={ctxQuery.tagIn}
-										initAuthorIds={ctxQuery.authorIn || []}
-										initKeyword={ctxQuery.search || ''}
-										onCategoriesUpdated={onCategoriesUpdated}
-										onTagsUpdated={onTagsUpdated}
-										onAuthorsUpdated={onAuthorsUpdated}
-										onKeywordUpdated={onKeywordUpdated}
-									/>
-									<div className="my-4 block w-full border-b border-neutral-300 lg:hidden dark:border-neutral-500" />
-									<div className="flex justify-end">
-										<ArchiveFilterListBox
-											defaultValue={
-												FILTERS_OPTIONS.filter(
-													(item) =>
-														item.value ==
-														`${ctxQuery.field.toUpperCase()}/${ctxQuery.order.toUpperCase()}`,
-												)[0]
-											}
-											onChange={(item) => {
-												const fiterValue = checkRouterQueryFilter(item.value)
-												if (!fiterValue) {
-													return
-												}
-												router.push({
-													query: removeEmptyKey({
-														...ctxQuery,
-														first: null,
-														last: null,
-														after: null,
-														before: null,
-														field: fiterValue.field,
-														order: fiterValue.order,
-													}),
-												})
-											}}
-											lists={FILTERS_OPTIONS}
+						<main>
+							{/* LOOP ITEMS */}
+							{!currentUsers.length && !loading ? (
+								<Empty />
+							) : (
+								<div className="mt-8 grid grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:mt-12 lg:grid-cols-3 xl:grid-cols-5">
+									{(currentUsers || []).map((user) => (
+										<CardAuthorBox
+											key={getUserDataFromUserCardFragment(user).databaseId}
+											author={user}
 										/>
-									</div>
+									))}
 								</div>
+							)}
 
-								{/* LOOP ITEMS */}
-								<GridPostsArchive
-									posts={initPosts}
-									showNextPagination={posts?.pageInfo.hasNextPage}
-									showPrevPagination={posts?.pageInfo.hasPreviousPage}
-									onClickNext={onClickNext}
-									onClickPrev={onClickPrev}
-								/>
-							</main>
-						</div>
-
-						{/* SUBCRIBES */}
+							{/* PAGINATION */}
+							{hasNextPage ? (
+								<div className="mt-12 flex justify-center lg:mt-14">
+									<ButtonPrimary
+										disabled={loading || !currentUsers?.length}
+										loading={loading}
+										onClick={handleClickShowMore}
+									>
+										{T['Show me more']}
+									</ButtonPrimary>
+								</div>
+							) : null}
+						</main>
 					</div>
+
+					{/* SUBCRIBES */}
 				</div>
-			</PageLayout>
-		</>
+			</div>
+		</PageLayout>
 	)
 }
 
-Page.variables = (context) => {
-	const { params, query = {} } = context as GetServerSidePropsContext
-	// query: { search: 'x', tagIn: ['1'], authorIn: ['2'], categoryIn: ['3'], page: '1', order: 'desc', field: 'date' }
-
-	const search = typeof query.search === 'string' ? query.search || null : null
-
-	const tagIn = (
-		Array.isArray(query.tagIn) ? query.tagIn : query.tagIn ? [query.tagIn] : []
-	).map((id) => parseInt(id))
-	const authorIn = (
-		Array.isArray(query.authorIn)
-			? query.authorIn
-			: query.authorIn
-				? [query.authorIn]
-				: []
-	).map((id) => parseInt(id))
-	const categoryIn = (
-		Array.isArray(query.categoryIn)
-			? query.categoryIn
-			: query.categoryIn
-				? [query.categoryIn]
-				: []
-	).map((id) => parseInt(id))
-
-	const field = typeof query.field === 'string' ? query.field : 'DATE'
-	const order = typeof query.order === 'string' ? query.order : 'DESC'
-
-	const after = typeof query.after === 'string' ? query.after || null : null
-	const before = typeof query.before === 'string' ? query.before || null : null
-
-	let first =
-		typeof query.first === 'string' ? parseInt(query.first) || null : null
-	let last =
-		typeof query.last === 'string' ? parseInt(query.last) || null : null
-
-	// lan dau request thi first = 20, last = null
-	if (!after && !before) {
-		first = GET_POSTS_FIRST_COMMON
-		last = null
-	}
-
-	const ctxQuery: ConTextQuery = {
-		...query,
-		search,
-		tagIn,
-		authorIn,
-		categoryIn,
-		field,
-		order,
-		first: last ? null : first || GET_POSTS_FIRST_COMMON,
-		last,
-		after,
-		before,
-	}
-
+export async function getStaticPaths() {
 	return {
-		...ctxQuery,
-		ctxQuery,
+		paths: [],
+		fallback: 'blocking',
+	}
+}
+export function getStaticProps(ctx: GetStaticPropsContext) {
+	return getNextStaticProps(ctx, {
+		Page,
+		revalidate: 900,
+	})
+}
+
+Page.variables = ({ params }) => {
+	return {
+		search: params?.search?.[0] || '',
+		first: GET_USERS_FIRST_COMMON,
 		headerLocation: PRIMARY_LOCATION,
 		footerLocation: FOOTER_LOCATION,
 	}
 }
 
 Page.query = gql(`
-  query PostsFilterPageQueryGetPosts( 
-	$in: [ID] = null
-	$first: Int = 20
-	$after: String = null
-	$last: Int
-	$before: String
-	$author: Int = null
-	$categoryId: Int = null
-	$authorIn: [ID] = null
-	$categoryIn: [ID] = null
-	$tagIn: [ID] = null
-	$categoryName: String = null
-	$tagId: String = null
-	$day: Int = null
-	$month: Int = null
-	$year: Int = null
-	$search: String = ""
-	$field: PostObjectsConnectionOrderbyEnum = DATE
-	$order: OrderEnum = DESC
-	$headerLocation: MenuLocationEnum!
-	$footerLocation: MenuLocationEnum!) {
-    posts(
-      first: $first
-      after: $after
-	  last: $last
-	  before: $before
-      where: {
-        in: $in
-		author: $author
-		authorIn: $authorIn
-		categoryIn: $categoryIn
-		tagIn: $tagIn
-		categoryId: $categoryId
-		categoryName: $categoryName
-		tagId: $tagId
-		dateQuery: { day: $day, month: $month, year: $year }
-		search: $search
-		orderby: { field: $field, order: $order }
-      }
-	) {
+  query AuthorsPageQueryGetUsersBySearch ( $first: Int,  $search: String = "", $after: String, $headerLocation: MenuLocationEnum!, $footerLocation: MenuLocationEnum! )  {
+    users(first: $first, after: $after, where: {search: $search}) {
         nodes {
-          ...NcmazFcPostCardFields
+             ...NcmazFcUserFullFields
         }
         pageInfo {
-			hasNextPage
-			endCursor
-			hasPreviousPage
-			startCursor
+          endCursor
+          hasNextPage
         }
-      }
-     
-    # common query for all page 
-    generalSettings {
+    }
+   # common query for all page 
+   generalSettings {
       ...NcgeneralSettingsFieldsFragment
     }
     primaryMenuItems: menuItems(where: { location:  $headerLocation  }, first: 80) {
@@ -368,11 +225,5 @@ Page.query = gql(`
     # end common query
   }
 `)
-
-export function getServerSideProps(ctx: GetServerSidePropsContext) {
-	return getNextServerSideProps(ctx, {
-		Page,
-	})
-}
 
 export default Page
