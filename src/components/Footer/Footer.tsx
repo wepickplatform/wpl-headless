5f08d54e892e9b5c38144ee0bd40b50bc5dd2460
wepickplatform <sanghyeon@wepick.kr>
2024-08-06 22:17:42 +0900

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -53,7 +53,7 @@ export default function Footer({ menuItems }: Props) {
 				Footer
 			</h2>
 			<div className="mx-auto max-w-7xl px-6 pb-8 pt-20 sm:pt-24 lg:px-8 lg:pt-28">
-				<div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24 dark:border-neutral-700">
+				<div className="mt-16 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24 dark:border-neutral-700">
 					<div style={{ flex: 1 }}>
 						<div style={{lineHeight: 2.0, display: 'flex', gap: '10px', marginBottom: '10px'}}>
 							<span className="mt-8 text-[13px] leading-5 text-black md:order-1 md:mt-0">위픽레터 소개</span>
