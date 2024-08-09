edb8fed36ff5030d6c401d8738260d485365c0ae
wepickplatform <sanghyeon@wepick.kr>
2024-08-09 18:36:39 +0900

    Update [[...search]].tsx

--- src/pages/authors/[[...search]].tsx
+++ src/pages/authors/[[...search]].tsx
@@ -33,7 +33,7 @@ const Page: FaustPage<AuthorsPageQueryGetUsersBySearchQuery> = (props) => {
         $search: String
         $after: String
       ) {
-        users(first: $first, after: $after, where: { search: $search, role: MARKETER }) {
+        users(first: $first, after: $after, where: { search: $search}) {
           nodes {
             ...NcmazFcUserFullFields
           }
@@ -194,7 +194,7 @@ Page.variables = ({ params }) => {
 
 Page.query = gql(`
   query AuthorsPageQueryGetUsersBySearch ( $first: Int,  $search: String = "", $after: String, $headerLocation: MenuLocationEnum!, $footerLocation: MenuLocationEnum! )  {
-    users(first: $first, after: $after, where: {search: $search}) {
+    users(first: $first, after: $after, where: {search: $search, role: MARKETER}) {
         nodes {
              ...NcmazFcUserFullFields
         }
