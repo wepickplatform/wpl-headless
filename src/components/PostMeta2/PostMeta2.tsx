fd6f079bb202835c2456dda72723ad54bb412ca1
wepickplatform <sanghyeon@wepick.kr>
2024-08-13 22:21:12 +0900

    Update PostMeta2.tsx

--- src/components/PostMeta2/PostMeta2.tsx
+++ src/components/PostMeta2/PostMeta2.tsx
@@ -83,7 +83,7 @@ const PostMeta2: FC<PostMeta2Props> = ({
 					</span>
 					<span className="mx-2 font-semibold">·</span>
 					<span className="text-neutral-700 dark:text-neutral-300">
-						{ncPostMetaData?.readingTime} min read
+						{ncPostMetaData?.readingTime} 분
 					</span>
 				</div>
 			</div>
