2cb100abbbe99aeb4015c48925b8472ddbb7ee9c
wepickplatform <sanghyeon@wepick.kr>
2024-08-22 11:29:21 +0900

    Update single-4.tsx

--- src/container/singles/single-4/single-4.tsx
+++ src/container/singles/single-4/single-4.tsx
@@ -6,21 +6,20 @@ import { getPostDataFromPostFragment } from "@/utils/getPostDataFromPostFragment
 interface Props extends SingleType1Props {}
 
 const SingleType4: FC<Props> = ({ post }) => {
-  const { title, content, date, author, databaseId, excerpt, featuredImage } =
-    getPostDataFromPostFragment(post || {});
-  //
   const postData = getPostDataFromPostFragment(post || {});
-  
+
   if (!postData) {
     return <div>Loading...</div>; // postData가 없을 경우 로딩 상태를 표시하거나 적절한 대체 UI를 표시
   }
+
+  const { title, content, date, author, databaseId, excerpt, featuredImage } = postData;
 
   return (
     <>
       <div className="absolute top-0 inset-x-0 bg-neutral-900 dark:bg-black/30 h-[480px] md:h-[600px] lg:h-[700px] xl:h-[95vh]"></div>
 
       <header className="container pt-10 lg:pt-16 rounded-xl relative z-10">
-        <SingleHeader4 post={post} />
+        <SingleHeader4 post={postData} /> {/* postData를 전달 */}
       </header>
     </>
   );
